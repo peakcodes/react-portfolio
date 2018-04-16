ddb26de721958fbabd10d90d105030ca24f96cb5
Tim Hill <peakcodes@gmail.com>
2018-04-15 23:40:40 -0400

    update

--- src/components/contact/contact.js
+++ src/components/contact/contact.js
@@ -16,8 +16,9 @@ class Contact extends Component {
                     My name is Tim Hill and I have dipped my pen in a number of different inks over the years. I have experinece in Agricultural Business, Enterprise Software Sales, Non Porfit Consulting, Blockchain, Distributed Ledger Technology and Web Development. All of these skills have come through many expierences with each buidling on the last to shape the professional I am today. One does not stop learning as life has become a continual learning proces.
                     When I have free time, I find myself outdoors or evaluating financial markets. I partiuclar like hiking, skiing, studying cryptocurrecy and reading. Furthermore, expanding my skillset and continuing my education.</p>
                     </Cell>
-                    <Cell col={6}>Contact Me
-                    
+                    <Cell col={6}>
+                    <h2> Contact Me <h2>
+                    <hr/>
                     </Cell>
                </Grid>
             </div>
